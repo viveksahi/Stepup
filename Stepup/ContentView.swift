7aae12fb5027759f5c1f07dc774b220ba7d0c513
Vvek Sahi <vwake.sahi@gmail.com>
2024-11-14 12:06:33 +0530

    Initial Commit

---
+++ Stepup/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Stepup
+//
+//  Created by Vivek Sahi on 14/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Stepup/StepupApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepupApp.swift
+//  Stepup
+//
+//  Created by Vivek Sahi on 14/11/24.
+//
+
+import SwiftUI
+
+@main
+struct StepupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
